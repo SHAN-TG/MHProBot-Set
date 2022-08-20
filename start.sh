a0e03be5c4933c03200e75864ffcef75f424c0a3
SHAN-TG <85439835+SHAN-TG@users.noreply.github.com>
2022-08-20 10:21:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
 git clone -b master https://github.com/SHAN-TG/MHProBot-Set.git /MHProBot-Set
-cd /IMDb-ALL-MH
+cd /MHProBot-Set
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
