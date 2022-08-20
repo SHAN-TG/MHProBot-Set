b3a96987621522a86ccd90de1717069132388870
SHAN-TG <85439835+SHAN-TG@users.noreply.github.com>
2022-08-20 10:20:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/SHAN-SN/IMDB-ALL-MH.git /IMDb-ALL-MH
+git clone -b master https://github.com/SHAN-TG/MHProBot-Set.git /MHProBot-Set
 cd /IMDb-ALL-MH
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
